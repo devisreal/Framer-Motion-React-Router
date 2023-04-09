4b9323c6821c93549bd6787a833032860cbd8b18
Israel Akinoso <akinosoissy@gmail.com>
2023-04-09 21:16:07 +0100

    Messing around with documentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import { useRef } from "react";
 import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
-import { motion, AnimatePresence } from "framer-motion"
+import { motion, AnimatePresence, useInView } from "framer-motion"
 import { useLocation } from "react-router-dom"
+import { stagger, animate } from "framer-motion"
+
+
 import './App.css'
 
 function LocationProvider({ children }) {
@@ -68,14 +72,52 @@ function App() {
 
 function Home() {
   return (
-    <motion.div
-      variants={routeVariants}
-      initial="initial"
-      animate="final"
-      className="home component"
-    >
-      <motion.h1 variants={childVariants} initial="initial" animate="final">  Home Component </motion.h1>
-    </motion.div>
+    <>
+      <motion.div
+        variants={routeVariants}
+        initial="initial"
+        animate="final"
+        className="home component"
+      >
+        <motion.h1 variants={childVariants} initial="initial" animate="final">  Home Component </motion.h1>
+        <motion.div
+          whileHover={{ scale: 1.2 }}
+          whileTap={{
+            scale: 0.8,
+            rotate: -90,
+            borderRadius: "100%"
+          }}
+          className="w-14 h-14 bg-white rounded"
+        >
+        </motion.div>
+        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+      </motion.div>
+
+      {/* 
+        <motion.div
+        initial={{
+          x: 200
+        }}
+        animate={{ x: 400 }}
+        transition={{ type: "spring", stiffness: 100 }}
+        className="w-32 h-32 my-20 mx-auto bg-red-300"
+      >
+
+      </motion.div>
+      */}
+      <motion.div
+        drag
+        dragConstraints={{ left: 0, right: 300, top: 0, bottom: 200 }}
+        dragElastic={1}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1, x: 200 }}
+        transition={{ type: "spring", stiffness: 100 }}
+        className="w-32 h-32 my-20 mx-auto bg-red-300"
+      >
+
+      </motion.div>
+    </>
   );
 }
 
@@ -93,15 +135,58 @@ function Header() {
 }
 
 function About() {
+  const ref = useRef(null);
+  const isInView = useInView(ref)
+  // animate(".about", { x: 100 }, { delay: stagger(0.1) })
   return (
-    <motion.div
-      variants={routeVariants}
-      initial="initial"
-      animate="final"
-      className="about component"
-    >
-      <motion.h1 variants={childVariants} initial="initial" animate="final"> About Component </motion.h1>
-    </motion.div>
+    <>
+      <motion.div
+        variants={routeVariants}
+        initial="initial"
+        animate="final"
+        className="about component"
+      >
+        <motion.h1 variants={childVariants} initial="initial" animate="final"> About Component </motion.h1>
+      </motion.div>
+      
+
+      <section ref={ref}>
+        <span
+          className="text-3xl"
+          style={{
+            transform: isInView ? "none" : "translateX(-200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.4s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+          }}
+        >
+          About
+        </span>
+      </section>
+      <section ref={ref}>
+        <span
+          className="text-3xl"
+          style={{
+            transform: isInView ? "none" : "translateX(-200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.4s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+          }}
+        >
+          About
+        </span>
+      </section>
+      <section ref={ref}>
+        <span
+          className="text-3xl"
+          style={{
+            transform: isInView ? "none" : "translateX(-200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.4s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+          }}
+        >
+          About
+        </span>
+      </section>
+    </>
   );
 }
 
